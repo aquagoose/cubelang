292a4518176c9331cb539a4121e8df08d0ea786e
ollie <ollie@ollierobinson.co.uk>
2023-12-01 14:33:53 +0000

    Initial commit, started lexer

---
+++ cubelang/src/lexer.rs
@@ -0,0 +1,99 @@
+pub enum Token {
+    Identifier(String),
+    Keyword(Keyword),
+    Number(f64),
+    String(String),
+
+    OpenParenthesis,
+    CloseParenthesis,
+
+    OpenBracket,
+    CloseBracket,
+
+    Plus,
+    Minus,
+    Asterisk,
+    ForwardSlash,
+
+    GreaterThan,
+    LessThan,
+
+    Comma,
+
+    Period,
+    Colon,
+
+    Eof
+}
+
+pub enum Keyword {
+    End,
+    Fn,
+    Data,
+    Var,
+    Return,
+    Is,
+    Not,
+    If,
+    Elif,
+    Else,
+    None,
+    True,
+    False
+}
+
+pub struct Lexer {
+    tokens: Vec<Token>
+}
+
+impl Lexer {
+    pub fn parse(code: &str) -> Self {
+        let chars = code.chars();
+        let mut current_pos = 0;
+
+        let mut tokens = Vec::new();
+
+        for (pos, c) in chars.enumerate() {
+            let token = match c {
+                '(' => Token::OpenParenthesis,
+                ')' => Token::CloseParenthesis,
+
+                '[' => Token::OpenBracket,
+                ']' => Token::CloseBracket,
+
+                '+' => Token::Plus,
+                '-' => Token::Minus,
+                '*' => Token::Asterisk,
+                '/' => Token::ForwardSlash,
+
+                '>' => Token::GreaterThan,
+                '<' => Token::LessThan,
+
+                ',' => Token::Comma,
+
+                '.' => Token::Period,
+                ':' => Token::Colon,
+
+                '\n' | '\r' | '\t' | ' ' => continue,
+
+                _ => {
+                    continue;
+                }
+            };
+
+            tokens.push(token);
+
+            current_pos = pos;
+        }
+
+        tokens.push(Token::Eof);
+
+        Self {
+            tokens
+        }
+    }
+
+    pub fn tokens(&self) -> &Vec<Token> {
+        &self.tokens
+    }
+}
